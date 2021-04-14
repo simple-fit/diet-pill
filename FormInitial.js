c4e8338dd3d19749bfcf82ca0128f901e12a8e7f
simple-fit <78045205+simple-fit@users.noreply.github.com>
2021-04-14 11:42:36 -0600

    Update FormInitial.js

--- FormInitial.js
+++ FormInitial.js
@@ -1,9 +1,10 @@
-(function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"17545445"};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");
+<script>(function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"17545445"};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");
 
 window.uetq = window.uetq || [];
 
 window.uetq.push('event', 'Checkout', {'event_category': 'BeginCheckout', 'event_label': 'FormInitial', 'event_value': '20.00'});
-
+</script>
+        <script>
 (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
         h._hjSettings={hjid:2292727,hjsv:6};
@@ -12,3 +13,4 @@ window.uetq.push('event', 'Checkout', {'event_category': 'BeginCheckout', 'event
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
         a.appendChild(r);
     })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
